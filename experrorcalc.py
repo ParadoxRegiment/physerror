2bd74f9fff85e87fc9c823b2a72243785133f2be
Alexander Ritzie <48335892+ParadoxRegiment@users.noreply.github.com>
2023-11-16 17:09:19 -0800

    Restructure/Rebuild of experrorcalc.py
    Completely restructured the module into 4 separate classes: Data, DataInit, calcs, and Graphs (names/capitalization tentative).
    - Data simply holds the most commonly reused data
    - DataInit asks if the user wishes to read in a csv, and does various things based off of that. The data from this method is placed into the self-reference data in the Data class.
    - calcs contains a large number of standard error calculations upon initialization, as well as a handful of methods also related to error calculation.
    - Graphs contains various methods that return graphs made via pyplot and pandas DataFrame methods, using the x_data and y_data that was passed into DataInit
    
    The commented out function is a test function for non-linear regression I am working on, and thus is nonfunctional at the moment.

--- experrorcalc.py
+++ experrorcalc.py
@@ -1,8 +1,10 @@
-#   #####################################   #  
-#    Module written by Alexander Ritzie     #
-#   Originally created for BPHYS 231 AU23   #
-#   at University of Washington - Bothell   #
-#   #####################################   #
+#       #####################################       #  
+#        Module written by Alexander Ritzie         #
+#       Originally created for BPHYS 312 AU23       #
+#       at University of Washington - Bothell       #
+#                   GitHub Repo:                    #
+#    https://github.com/ParadoxRegiment/BPHYS231    #
+#       #####################################       #
 import numpy as np
 from matplotlib import pyplot as plt
 import scipy.stats as stats
@@ -10,17 +12,72 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import pandas as pd
 
-class Calculations:
-    """
-    To access array values and functions do:
-    [variable] = Calculuations(ndarray[optional], ndarray[optional])
-    
-    To access methods, do:
-    [above variable].[method name]
+class Data:
+    def __init__(self):
+        self.x_data = np.array([])
+        self.y_data = np.array([])
+        self.df = pd.DataFrame()
+        self.colname1 = ''
+        self.colname2 = ''
 
-    -----------------------------------------------
+class DataInit:
+    def __init__(self, x_data = np.full(5,5), y_data = np.full(5,5)):
+        global userdata
+        userdata = Data()
+        readcsv = input("Would you like to read in a CSV? Y/N\n")
+        if readcsv.lower() == "y" or readcsv.lower() == "yes":
+            print("Please choose a csv file:")
+            tk = Tk()
+            tk.withdraw()
+            path = askopenfilename()
+            print(path)
+            with open(path, "r") as f:
+                datafile = pd.read_csv(f, header = None, index_col = None)
+                colcount = len(datafile.axes[1])
+                dataarray = np.array(datafile)
+
+            if np.ndim(dataarray) == 1:
+                print("csv read into x_data")
+                x_data = dataarray
+            elif np.ndim(dataarray) > 1:
+                if colcount == 1:
+                    print("csv read into x_data")
+                    x_data = dataarray[:,0]
+                elif colcount > 1:
+                    x_data = dataarray[:,0]
+                    y_data = dataarray[:,1]
+        elif readcsv.lower() == "n" or readcsv.lower() == "no":
+            pass
+        else:
+            print("Unknown input, please restart.")
+            exit()
+        
+        global testarray
+        testarray = np.full(5,5)
+        
+        if np.array_equiv(testarray, y_data):
+            y_data = np.full_like(x_data, 5)
+        
+        temparray = np.stack((x_data, y_data))
+        userdata.colname1 = input("Please type first data set's name: ")
+        userdata.colname2 = input("Please type second data set's name: ")
+        datafile = pd.DataFrame(np.transpose(temparray), columns = [userdata.colname1, userdata.colname2])
+        datafile.index.name = 'Trial'
+        
+        print(datafile)
+
+        userdata.df = datafile
+        userdata.x_data = x_data
+        userdata.y_data = y_data
+        self.colname1 = userdata.colname1
+        self.colname2 = userdata.colname2
+
+class calcs:
+    """
+    To access attributes, must do [variable] = calcs(ndarray, ndarray)
+    To access methods, do [above variable].[function name]
     
-    Available array values:
+    Available attributes:
         N: size of x data
         delta: the calculated delta constant
         A: the calculated A constant
@@ -39,16 +96,13 @@ class Calculations:
         sigma_y_mean: the calculated sigma of the y data mean
         sigma_frac: the calculated fractional uncertainty
     """
-    
-    def __init__(data, x_data = np.full(5,5), y_data = np.full(5,5)):
+    def __init__(self):
         """
         Initalization function. x_data and y_data are initalized to
         arrays of size 5 filled with 5s.
         
         For csv data files, please make sure your x data is in the first
-        row, and y data is in the second row. Also mark the first column
-        with data labels such as "x" and "y", "1" and "2", "test 1" and
-        "test 2", so on and so forth.
+        row, and y data is in the second row.
         
         Parameters:
             x_data : ndarray [optional]
@@ -57,68 +111,36 @@ class Calculations:
                 The desired y_data array. Initalized to np.full(5,5)
         """
         
-        
-        readcsv = input("Would you like to read in a CSV? Y/N\n")
-        if readcsv.lower() == "y" or readcsv.lower() == "yes":
-            print("Please choose a csv file:")
-            tk = Tk()
-            tk.withdraw()
-            path = askopenfilename()
-            print(path)
-            with open(path, "r") as f:
-                datafile = np.array(pd.read_csv(f, header = None, index_col = 0))
-
-            if np.ndim(datafile) == 1:
-                print("csv read into x_data")
-                x_data = datafile
-            elif np.ndim(datafile) > 1:
-                x_data = datafile[0]
-                y_data = datafile[1]
+        self.df = userdata.df
+        self.x_data = userdata.x_data
+        self.y_data = userdata.y_data
+        self.N = len(userdata.x_data)
+        self.delta = self.N * sum(userdata.x_data ** 2) - (sum(userdata.x_data)) ** 2
+        self.A = ((sum(userdata.x_data ** 2) * sum(userdata.y_data)) - (sum(userdata.x_data) * sum(userdata.x_data * userdata.y_data))) / self.delta
+        self.B = (self.N * sum(userdata.x_data * userdata.y_data) - (sum(userdata.x_data) * sum(userdata.y_data))) / self.delta
+        self.x_mean = abs(np.mean(userdata.x_data))
+        self.x_best = sum(userdata.x_data)/self.N
+        self.y_mean = abs(np.mean(userdata.y_data))
+        self.y_best = sum(userdata.y_data)/self.N
+        self.sigma_y = np.sqrt((1/(self.N - 2)) * sum((userdata.y_data - self.A - (self.B * userdata.x_data)) ** 2))
+        self.sigma_A = self.sigma_y * np.sqrt(sum(userdata.x_data ** 2) / self.delta)
+        self.sigma_B = self.sigma_y * np.sqrt(self.N / self.delta)
+        self.sigma_x = np.sqrt(sum((userdata.x_data - self.x_mean) ** 2) / (self.N - 1))
+        self.sigma_x_best = np.sqrt((1/(self.N - 1)) * sum((userdata.x_data - self.x_mean) ** 2))
+        self.sigma_y_best = np.sqrt((1/(self.N - 1)) * sum((userdata.y_data - self.y_mean) ** 2))
+        self.sigma_x_mean = self.x_mean / np.sqrt(self.N)
+        self.sigma_y_mean = self.y_mean / np.sqrt(self.N)
+        self.sigma_frac = 1 / np.sqrt(2 * (self.N - 1))
     
-        elif readcsv.lower() == "n" or readcsv.lower() == "no":
-            pass
-        else:
-            print("Unknown input, please restart.")
-            exit
-        
-        global testarray
-        testarray = np.full(5,5)
-        
-        if np.array_equiv(testarray, y_data):
-            y_data = np.full_like(x_data, 5)
-
-        data.x_data = x_data
-        data.y_data = y_data
-        data.N = len(x_data)
-        data.delta = data.N * sum(x_data ** 2) - (sum(x_data)) ** 2
-        data.A = ((sum(x_data ** 2) * sum(y_data)) - (sum(x_data) * sum(x_data * y_data))) / data.delta
-        data.B = (data.N * sum(x_data * y_data) - (sum(x_data) * sum(y_data))) / data.delta
-        data.x_mean = abs(np.mean(x_data))
-        data.x_best = sum(x_data)/data.N
-        data.y_mean = abs(np.mean(y_data))
-        data.y_best = sum(y_data)/data.N
-        data.sigma_y = np.sqrt((1/(data.N - 2)) * sum((y_data - data.A - (data.B * x_data)) ** 2))
-        data.sigma_A = data.sigma_y * np.sqrt(sum(x_data ** 2) / data.delta)
-        data.sigma_B = data.sigma_y * np.sqrt(data.N / data.delta)
-        data.sigma_x = np.sqrt(sum((x_data - data.x_mean) ** 2) / (data.N - 1))
-        data.sigma_x_best = np.sqrt((1/(data.N - 1)) * sum((x_data - data.x_mean) ** 2))
-        data.sigma_y_best = np.sqrt((1/(data.N - 1)) * sum((y_data - data.y_mean) ** 2))
-        data.sigma_x_mean = data.x_mean / np.sqrt(data.N)
-        data.sigma_y_mean = data.y_mean / np.sqrt(data.N)
-        data.sigma_frac = 1 / np.sqrt(2 * (data.N - 1))
-    
-    def outlier(data):
+    def outlier(self):
         """
             Checks for and prints out any outliers in included arrays within the 2 * sigma limit.
             
             Returns: print()
                         Prints out x_outliers and y_outliers in the form of ndarrays
         """
-        x_data = data.x_data
-        y_data = data.y_data
-        
-        if np.array_equiv(testarray, y_data):
-            y_data = np.full_like(x_data, 5)
+        x_data = self.x_data
+        y_data = self.y_data
         
         print("Given x data:", x_data)
         print("Given y data:", y_data)
@@ -131,22 +153,22 @@ class Calculations:
         l = 0
         
         for row in x_data:
-            if row > (data.x_mean + 2 * data.sigma_x):
+            if row > (self.x_mean + 2 * self.sigma_x):
                 x_outliers[j] = int(row)
                 j += 1
                 x_data = np.delete(x_data, i)
-            elif row < (data.x_mean - 2 * data.sigma_x):
+            elif row < (self.x_mean - 2 * self.sigma_x):
                 x_outliers[j] = int(row)
                 j += 1
                 x_data = np.delete(x_data, i)
             i += 1
             
         for row in y_data:
-            if row > (data.y_mean + 2 * data.sigma_y):
+            if row > (self.y_mean + 2 * self.sigma_y):
                 y_outliers[l] = int(row)
                 l += 1
                 y_data = np.delete(y_data, k)
-            elif row < (data.y_mean - 2 * data.sigma_y):
+            elif row < (self.y_mean - 2 * self.sigma_y):
                 y_outliers[l] = int(row)
                 l += 1
                 y_data = np.delete(y_data, k)
@@ -163,29 +185,33 @@ class Calculations:
 
         print(x_outliers)
         print(y_outliers)
-        
-    def regress(data):
+
+class Graphs:
+    def __init__(self):
+        global calcsclass
+        calcsclass = calcs()
+    
+    def regress(self):
         """
             Uses the given x_data and y_data arrays to create a linear regression plot.
             
             Returns: plt.show()
                         Opens an external window that shows the linear regression plot of the given data. 
         """
-        x_data = data.x_data
-        y_data = data.y_data
+        x_data = userdata.x_data
+        y_data = userdata.y_data
         
         if np.array_equiv(testarray, y_data):
             y_data = np.full_like(x_data, 5)
         
-        print("Given x data:", x_data)
-        print("Given y data:", y_data)
-        
-        plt.title('pyplot best fit & linear regression plot')
+        plt.title('pyplot best fit & linear regression plot', fontsize = 14)
         plt.plot(x_data, y_data, 'o')
-        plt.plot(x_data, data.A + data.B * x_data)
+        plt.xlabel(userdata.colname1, fontsize = 14)
+        plt.ylabel(userdata.colname2, fontsize = 14)
+        plt.plot(x_data, calcsclass.A + calcsclass.B * x_data)
         plt.show()
-    
-    def standdistgraph(data):
+        
+    def standdistgraph(self):
         """
             Uses the given x_data array to create a standard distribution graph.
             Currently only works for x data.
@@ -193,17 +219,86 @@ class Calculations:
             Returns: plt.show()
                         Opens an external window that shows the standard distribution graph
         """
-        x_data = data.x_data
-        y_data = data.y_data
+        x_data = userdata.x_data
+        y_data = userdata.y_data
         
         gdata = x_data
 
         plt.hist(gdata, bins = len(gdata), density = True, alpha = 0.6, color = 'c', align = 'mid')
         xmin, xmax = plt.xlim()
         x = np.linspace(xmin, xmax, 100)
-        p = stats.norm.pdf(x, data.x_mean, data.sigma_x)
+        p = stats.norm.pdf(x, calcsclass.x_mean, calcsclass.sigma_x)
         plt.plot(x, p, 'k', linewidth = 2)
-        title = "x data mean = %.2f, stddev x = %.2f, 2 stddev x = %.2f" % (data.x_mean, data.sigma_x, 2 * data.sigma_x)
+        title = "x data mean = %.2f, stddev x = %.2f, 2 stddev x = %.2f" % (calcsclass.x_mean, calcsclass.sigma_x, 2 * calcsclass.sigma_x)
         plt.title(title)
         plt.show()
+    
+    def errbargraph(self, x_axis = "y_data", y_axis = "x_data", gtitle = "graph"):
+        """
+            Uses the given dataframe built from x_data and y_data during
+            initalization to create an error bar plot, making use of
+            the sigma_x value as the constant error.
+            
+            Parameters:
+                x_axis : str [optional]
+                    The desired label for the x axis. Is initalized to
+                    "y_data" (as most datasets will have the y data
+                    in the second column)
+                y_axis : str [optional]
+                    The desired label for the y axis. Is initalized to
+                    "x_data" (as most data sets will have the x data
+                    in the first column)
+                gtitle : str [optional]
+                    The desired graph title. is initalized to "graph"
+            
+            Returns: plt.show()
+                        Opens an external window that displays the
+                        error bar graph.
+        """
+        # df = data.df.set_axis(['X', 'Y'], axis = 1)
+        df = userdata.df
+        df.plot(x = userdata.colname2, y = userdata.colname1,
+                xlabel = x_axis, ylabel = y_axis, title = gtitle,
+                linestyle = "", marker = ".", yerr = calcsclass.sigma_x,
+                capthick = 1, ecolor = "red", linewidth = 1)
+        plt.show()
+    
+    def datahist(self):
+        datafile = userdata.df
+        columns = datafile.columns
+        histcheck = input("Do you want a histogram for 1 dataset, or 2 datasets? ")
         
+        try:
+            int(histcheck)
+        except ValueError:
+            print("Please input only numerical values 1 or 2.")
+            exit()
+            
+        if int(histcheck) == 1:
+            print("Which dataset would you like to use?", userdata.colname1, "or", userdata.colname2,)
+            histnum = input()
+            if histnum == userdata.colname1:
+                datafile.hist(bins = len(datafile.axes[0]), grid = False, rwidth = .9,
+                                         column = columns[0], color = 'green')
+            elif histnum == userdata.colname2:
+                datafile.hist(bins = len(datafile.axes[0]), grid = False, rwidth = .9,
+                                         column = columns[1], color = 'green')
+            else:
+                print("Please input only", userdata.colname1, "or", userdata.colname2)
+                exit()
+        elif int(histcheck) == 2:
+            fig, axes = plt.subplots(nrows = 2, ncols = 1)
+            datafile.hist(bins = len(datafile.axes[0]), grid = False, rwidth = .9,
+                                     column = columns[0], color = 'green', ax = axes[0])
+            datafile.hist(bins = len(datafile.axes[1]), grid = False, rwidth = .9,
+                                     column = columns[1], color = 'c', ax = axes[1])
+        else:
+            print("Please input only 1 or 2.")
+            exit
+        plt.show()
+        
+    # def nonlinregress(data):
+    #     df = data.df
+    #     # container = np.array([])
+    #     def f_model(x, a, c):
+    #         return np.log(np.array(((a + x) ** 2) / ((x - c) ** 2)))
